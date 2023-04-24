75ad2e8c463b37a0b4b8e381abec3eae2a1d1593
Mat Dupont <dupont.mat.l@gmail.com>
2023-04-24 13:02:00 -0400

    Test comments and cleanup
    sadasdsd

--- TASK_3/tests/api.spec.js
+++ TASK_3/tests/api.spec.js
@@ -3,29 +3,33 @@ const { test, expect } = require('@playwright/test');
 
 import * as credentials from '../infoForTests/credentials.json';
 
-test ('test1Cookies', async ({page}) => {
+test ('Authenticated Book Store Flow', async ({page}) => {
+  // Login
   await page.goto('https://demoqa.com/login');
   await page.getByPlaceholder('UserName').fill(credentials.userName);
   await page.getByPlaceholder('Password').fill(credentials.password);
   await page.locator('#login').click();
+  
   const locator = page.locator('.main-header');
   await expect(locator).toHaveText('Profile');
   await page.waitForURL('https://demoqa.com/profile');
 
+  
+  // Checking cookies
   const cookies = await page.context().cookies();
-  for (const cookie of cookies) {
-    console.log(JSON.stringify(cookie))
-  }
-
-  //check that cookies are not empty
-  await expect(cookies.find(c => c.name == 'userID').value).toBeTruthy();
-  await expect(cookies.find(c => c.name == 'userName').value).toBeTruthy();
-  await expect(cookies.find(c => c.name == 'expires').value).toBeTruthy();
-  await expect(cookies.find(c => c.name == 'token').value).toBeTruthy();
 
   //save variables
   const userID = cookies.find(c => c.name == 'userID').value;
   const token = cookies.find(c => c.name == 'token').value;
+  
+  //check that cookies are not empty
+  await expect(userID).toBeTruthy();
+  await expect(token).toBeTruthy();
+  await expect(cookies.find(c => c.name == 'userName').value).toBeTruthy();
+  await expect(cookies.find(c => c.name == 'expires').value).toBeTruthy();
+
+
+  
 
   // block images
   await page.route('**/*', (route) => {
